1a43b39fa4f71b529808dca38c89c34a101c8bb3
carlosocarvalho <>
2019-08-13 01:46:49 -0300

    Updates

---
+++ precache-manifest.e6b5cdeba0f34beec3451d126c35cec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031f2ee15bebbac56e36eab31cb67529",
+    "url": "/reactbox/index.html"
+  },
+  {
+    "revision": "b1a2332f755ee671f80e",
+    "url": "/reactbox/static/css/main.1377ba56.chunk.css"
+  },
+  {
+    "revision": "ac3a2b688698b07768e8",
+    "url": "/reactbox/static/js/2.e46e3bee.chunk.js"
+  },
+  {
+    "revision": "b1a2332f755ee671f80e",
+    "url": "/reactbox/static/js/main.b7b33958.chunk.js"
+  },
+  {
+    "revision": "9e0cb6f3a9568b7c6651",
+    "url": "/reactbox/static/js/runtime~main.4050c005.js"
+  }
+]);
